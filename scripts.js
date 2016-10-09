96fdc4c07ff97ad6db45fd3a2807064203e8e038
mrcopprhead <qwertyjcleveland@gmail.com>
2016-10-09 10:55:53 -0500

    file

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $.getJSON("https://raw.githubusercontent.com/GETHIP-Classroom/2017-hw2-MrCopprHead/master/logan.json", function(data){
+  $.getJSON("https://raw.githubusercontent.com/GETHIP-Classroom/2017-hw2-MrCopprHead/master/jacob.json", function(data){
     console.log(data);
     $("#profile-name").text(data.firstName + " " + data.lastName);
     $("#age").text(data.age);
